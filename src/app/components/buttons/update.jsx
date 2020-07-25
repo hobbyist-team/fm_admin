52b4b2127b944b39a67e65c69ce52831d9b893b5
yuvigrg <yuwaraj.gurung@outlook.com>
2020-07-25 20:45:59 +0100

    Extract the button functionality to its components

---
+++ src/app/components/buttons/update.jsx
@@ -0,0 +1,54 @@
+import React, { useCallback } from 'react';
+import { Button } from '@material-ui/core';
+import axios from 'axios';
+import { useSnackbar } from 'notistack';
+import { func, shape, string, bool } from 'prop-types';
+
+const UpdateButton = ({ setSubmitting, record, disabled }) => {
+  const { enqueueSnackbar, closeSnackbar } = useSnackbar();
+
+  const submit = useCallback((event) => {
+    if (event) event.preventDefault();
+    closeSnackbar();
+    setSubmitting(true);
+    const { id, title, frequency, url, imageUrl } = record;
+
+    axios.patch(`/api/${id}?title=${title}&&frequency=${frequency}&&url=${url}&&imageUrl=${imageUrl}`)
+      .then((res) => {
+        if (res) {
+          enqueueSnackbar('Successfully Updated');
+        } else {
+          enqueueSnackbar('Oops! Something gone wrong');
+        }
+      })
+      .catch(() => {
+        enqueueSnackbar('Oops! Something gone wrong');
+      });
+    setSubmitting(false);
+  }, [closeSnackbar, setSubmitting, record, enqueueSnackbar]);
+
+  return (
+    <Button
+      type="submit"
+      color="primary"
+      onClick={submit}
+      disabled={disabled}
+    >
+      Update Record
+    </Button>
+  );
+};
+
+UpdateButton.propTypes = {
+  setSubmitting: func.isRequired,
+  record: shape({
+    id: string,
+    title: string,
+    frequency: string,
+    url: string,
+    imageUrl: string,
+  }).isRequired,
+  disabled: bool.isRequired,
+};
+
+export default UpdateButton;
